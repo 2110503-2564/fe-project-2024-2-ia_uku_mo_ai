0cb23051f0a049604cb45dbf8041e5a6202d4a40
Xchi <138505064+idknowwhy123@users.noreply.github.com>
2025-03-23 12:09:23 +0700

    modified callbackUrl

--- src/app/api/auth/[...nextauth]/authOption.ts
+++ src/app/api/auth/[...nextauth]/authOption.ts
@@ -39,5 +39,8 @@ export const authOptions: AuthOptions = {
           }
           return session;
         },
+        async redirect({ url, baseUrl }) {
+          return url.startsWith(baseUrl) ? url : baseUrl + "/";
+        }
       },
     };
